9d81577a98d823327bcc0eea134d632573f59f8b
kemvanra ouch <ouchkemvanra1@gmail.com>
2021-10-26 10:34:09 +0700

    Initial project setup

---
+++ MarioExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarioExamples
+//
+//  Created by Ouch Kemvanra on 10/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
