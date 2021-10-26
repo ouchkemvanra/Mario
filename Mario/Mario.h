392e0805e2ef9129870f5e7ed10c8f7436bc20d1
kemvanra ouch <ouchkemvanra1@gmail.com>
2021-10-26 10:11:45 +0700

    Initial Commit

---
+++ Mario/Mario.h
@@ -0,0 +1,18 @@
+//
+//  Mario.h
+//  Mario
+//
+//  Created by Ouch Kemvanra on 10/26/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Mario.
+FOUNDATION_EXPORT double MarioVersionNumber;
+
+//! Project version string for Mario.
+FOUNDATION_EXPORT const unsigned char MarioVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mario/PublicHeader.h>
+
+
